8877754948dc99e3880fc53ad9ba653769e52861
AlbertoJoseDAM <jamartin@itpfp.com>
2023-12-01 12:44:01 +0100

    first commit

---
+++ src/main/java/com/mycompany/entornopreguntas/EntornoPreguntas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.entornopreguntas;
+
+/**
+ *
+ * @author Alumno
+ */
+public class EntornoPreguntas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
